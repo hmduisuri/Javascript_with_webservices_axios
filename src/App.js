50c1f4b57c2eac62e73b705ac08ffb423f3c7376
hmduisuri <hmduisuri@gmail.com>
2021-01-08 22:47:30 +0900

    add lazy route using AsyncComponent

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      //if you use sub directory when deplying the app base name should set
+      // <BrowserRouter basename = "my-app"/>
       <BrowserRouter>
         <div className="App">
           <Blog />
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,13 +3,19 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 //nav link is used to style active link in various ways : and 'active' class is te default classname of it . if you want to change, change asa below
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 // import FullPost from './FullPost/FullPost';
-// import axios from 'axios';
-// import axios from '../../axios'
+
+//see network tab| import newPost dynemically -- lazy loading -- load only AsyncNewPost is called somewhere// that file, loading into the chunk file instead of main bundle
+const AsyncNewPost = asyncComponent(() => {
+    return import ('./NewPost/NewPost');
+});
 
 class Blog extends Component {
-
+state = {
+    auth: true
+}
     render () {
         return (
             <div className = "Blog">
@@ -36,9 +42,12 @@ class Blog extends Component {
                 {/* exact is used to path which is only localhost:8080/ 'for / only' */}
                 {/* in this case page is re-loading not rendering part of the dom */}
                 <Switch>
-                <Route path = "/new-post" component = {NewPost}/>
+                    {/* swith statements are running orderly */}
+                {this.state.auth? <Route path = "/new-post" component = {AsyncNewPost}/>:null}
                 <Route path = "/posts"  component = {Posts}/>    
                 <Redirect from = "/" to = "/posts"/>
+                {/* catch unknown routes */}
+                {/* <Route render = {() => <h1 style={{textAlign: 'center'}}>Page not found!</h1>}/> */}
                 </Switch>
             </div>
         );
